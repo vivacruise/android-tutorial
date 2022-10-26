d67f1e6d8bd5dfbdb4f63a16ff378799a071520f
sswuss <sswuss@outlook.com>
2022-10-26 18:31:23 +0800

    删除

--- gitHubTest/src/main/kotlin/Main.kt
+++
@@ -1,7 +0,0 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
-    println("Hello GitHub!")
-    println("Hello GitHub!")
-    println("Hello GitHub!")
-    println("Hello GitHub!")
-}
