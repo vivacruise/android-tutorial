eb11f2ae52ba50c1d44d679697a04cb455f380ab
sswuss <sswuss@outlook.com>
2022-10-26 18:30:46 +0800

    Update Main.kt

--- gitHubTest/src/main/kotlin/Main.kt
+++ gitHubTest/src/main/kotlin/Main.kt
@@ -1,4 +1,7 @@
 fun main(args: Array<String>) {
     println("Hello World!")
     println("Hello GitHub!")
+    println("Hello GitHub!")
+    println("Hello GitHub!")
+    println("Hello GitHub!")
 }
